3c8edf1f9d16a91492715d77ba45b6f7f7d4054c
David Spence <d.spence@kainos.com>
2015-09-03 11:16:29 +0100

    Updating pom and main class to select from pet table

--- my-app/src/main/java/com/spencerooni/app/App.java
+++ my-app/src/main/java/com/spencerooni/app/App.java
@@ -1,13 +1,43 @@
 package com.spencerooni.app;
 
-/**
- * Hello world!
- *
- */
+import java.sql.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+            System.out.println(ex);
+        }
+
+        Connection conn = null;
+
+        try
+        {
+            String url = "jdbc:mysql://localhost/test";
+            String user = "root";
+            String password = "ch@ngeme1";
+
+            conn = DriverManager.getConnection(url, user, password);
+
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from pet");
+
+            while(rs.next()) {
+                String out = String.format("name: %s, sex: %s", rs.getString("name"), rs.getString("sex"));
+                System.out.println(out);
+            }
+
+            st.close();
+            conn.close();
+        }
+        catch(SQLException e) {
+            System.out.println(e);
+        }
     }
 }
